a1b7c71fe53fdeb40d7cd646fb0e29bd769c3483
Carlos magno <cmagnobarbosa@gmail.com>
2025-01-11 22:24:42 -0300

    Add method to handler OpenAI error

--- core/authentication.py
+++ core/authentication.py
@@ -6,6 +6,7 @@ from fastapi import Header, HTTPException
 
 API_TOKEN = os.getenv("UAI_API_TOKEN")
 
+
 # Função de dependência para verificação do token
 def verificar_token(authorization: str = Header(...)):
     """Verifica se o token de autorização é válido.
